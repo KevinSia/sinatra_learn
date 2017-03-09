bb30a3093713ef40fa82116a607d7d06720c0003
KevinSia <kevinsiakeemun@gmail.com>
2017-03-09 10:38:16 +0800

    can submit post

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,29 +1,20 @@
+# method: post
+# action: '/create_url'
+
 # /posts/3
 get '/' do
   erb :"static/index"
 end
 
-get '/contacts' do
-  erb :"static/contacts"
+post '/create_url' do
+  post = Post.new(title: params[:title], content: params[:content])
+  post.capitalize!
+  post.save
+
+  redirect '/'
 end
 
 get '/posts/:id' do
-  byebug
   @post = Post.find(params[:id])
   erb :"posts/show"
 end
-
-# get '/posts/1' do
-#   @post = Post.first
-#   erb :"posts/show"
-# end
-#
-# get '/posts/2' do
-#   @post = Post.second
-#   erb :"posts/show"
-# end
-#
-# get '/posts/3' do
-#   @post = Post.find(3)
-#   erb :"posts/show"
-# end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+  def capitalize!
+    self.title.capitalize!
+    self.content.capitalize!
+  end
 end
